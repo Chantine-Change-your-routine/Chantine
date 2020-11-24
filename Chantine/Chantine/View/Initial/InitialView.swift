896b11b546a498ebe15a1c66957236b1ee992980
Beatriz Carlos <beatrizcarlos2000@gmail.com>
2020-11-24 09:49:54 -0300

    adding assets and the colors of the app.

---
+++ Chantine/Chantine/View/Initial/InitialView.swift
@@ -0,0 +1,20 @@
+//
+//  InitialView.swift
+//  Chantine
+//
+//  Created by Beatriz Carlos on 23/11/20.
+//
+
+import UIKit
+
+class InitialView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
